d96b2b3bb96b576e167dd81c7cfc40dd3fd1c494
William Falk-Wallace <wgf2104@columbia.edu>
2015-02-05 11:40:36 -0800

    readds as content

---
+++ instagram/ObjectiveCBridge.m
@@ -0,0 +1,10 @@
+//
+//  ObjectiveCBridge.m
+//  instagram
+//
+//  Created by Aditya Jayaraman on 2/4/15.
+//  Copyright (c) 2015 copdepath. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AFNetworking.h"
